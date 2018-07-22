0839c10187fc2a751a2395d09d5f9ebcb6c8b413
Maria Clara Dias <maria.diaas@gmail.com>
2018-07-22 18:06:12 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,27 +1,10 @@
 <?php
-/*
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-// Staging and Production
-$db['default'] = array(
-    'hostname'      =>  $url["host"],
-    'username'      =>  $url["user"],
-    'password'      =>  $url["pass"],
-    'database'      =>  substr($url["path"], 1),
-    'dbdriver'      =>  'mysqli',
-    'activer'      =>  true,
-    'pconnect'      =>  false,
-    'dbdebug'      =>  false,
-    'cacheon'      =>  false,
-    'charset'      =>  'utf8',
-    'dbcollat'      =>  'utf8unicodeci',
-);*/
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.185.213.226;dbname=bertoldi_maria_clara',
-    'username' => 'bertoldi_maria_c',
-    'password' => 'maria@clara',
+    'dsn' => 'mysql:host=getenv('host');dbname=getenv('dataBase')',
+    'username' => 'getenv('username')',
+    'password' => 'getenv('password')',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
